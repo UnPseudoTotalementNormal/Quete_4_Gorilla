4a63ac67b2a91d5df21aa3da60f9bccf36c236bf
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2023-11-26 01:42:40 +0100

    Better parallax (y axis works now)

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Vector2 cloudoffset = Vector2.zero;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Camera.main.transform.position.x - transform.position.x, 0, 0);
+        transform.position += new Vector3(Camera.main.transform.position.x - transform.position.x, Camera.main.transform.position.y - transform.position.y, 0);
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform background = transform.GetChild(i);
-            float dist = Camera.main.transform.position.x;
+            float distx = Camera.main.transform.position.x;
+            float disty = Camera.main.transform.position.y;
             if (background.tag == "Cloud")
             {
                 cloudoffset += ((Vector2.right * 2) / background.transform.position.z) * Time.deltaTime;
-                background.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", Vector2.right * dist / background.transform.position.z + cloudoffset);
+                background.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", (Vector2.right * distx / background.transform.position.z) + (Vector2.up * disty / background.transform.position.z) + cloudoffset);
                 continue;
             } 
-            background.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", Vector2.right * dist / background.transform.position.z);
+            background.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", (Vector2.right * distx / background.transform.position.z) + (Vector2.up * disty / background.transform.position.z));
         }
     }
 }
